81a2f718f826a53f035cf650a609d69c14f2bf06
Alberto Casares <better.acs@gmail.com>
2024-11-03 18:05:42 +0100

    CI

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,8 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  force_ssl_env = ENV.fetch("FORCE_SSL", "true")
+  config.force_ssl = force_ssl_env == "true"
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
